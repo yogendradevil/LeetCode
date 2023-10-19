646dc3ee953fc39615d34fdb31d549bc38aa1f7a
Yogendra Bijapur <81254268+yogendradevil@users.noreply.github.com>
2023-10-20 02:35:05 +0530

    Time: 1084 ms (63.46%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,6 @@
+select 
+project_id,
+round(sum(e.experience_years) / count(e.experience_years),2) as average_years
+from Project p
+join Employee e on p.employee_id = e.employee_id
+group by project_id
